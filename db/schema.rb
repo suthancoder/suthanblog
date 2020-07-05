ebeb4652e0ced40b9f29340fcbc52b21cc0cad44
ambasuthanjananayagam <ambasuthanjananayagam@yahoo.co.uk>
2020-07-06 00:45:44 +0100

    skill models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_05_171616) do
+ActiveRecord::Schema.define(version: 2020_07_05_234057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2020_07_05_171616) do
     t.string "title"
     t.text "body"
     t.text "keywords"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.integer "percent_utilized"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
