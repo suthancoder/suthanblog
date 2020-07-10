b5dc689fa2254f69f53a2843b75f5caf289b9d89
ambasuthanjananayagam <ambasuthanjananayagam@yahoo.co.uk>
2020-07-10 23:01:52 +0100

    custom routes done

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,62 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @portfolio_items = Portfolio.all
+  end
+
+  def new
+    @portfolio = Portfolio.new
+  end
+
+  def create
+    @portfolio = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+        format.json { render :show, status: :created, location: @portfolio }
+      else
+        format.html { render :new }
+        format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+        respond_to do |format|
+      if @portfolio.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+        format.json { render :show, status: :ok, location: @portfolio }
+      else
+        format.html { render :edit }
+        format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+
+  def destroy
+    @portfolio.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Portfolio item was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+
+private
+
+  def set_portfolio
+      @portfolio = Portfolio.find(params[:id])
+  end
+
+  def portfolio_params
+        params.require(:portfolio).permit(:title, :subtitle, :body)
+  end
+
 end
