ec774be8498e45cdd4a5f258f460276961d699c7
ambasuthanjananayagam <ambasuthanjananayagam@yahoo.co.uk>
2020-11-26 20:42:04 +0000

    worked on custom layouts

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,6 @@
 class PortfoliosController < ApplicationController
   before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
-
+  layout "portfolio"
   def index
     @portfolio_items = Portfolio.all
   end
